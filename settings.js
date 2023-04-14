77b74cb53518d6caf297db581a08be06517949ad
Anonymous <92008777+zeeoneofficial@users.noreply.github.com>
2023-04-14 16:02:20 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "5987177009:AAGljRbJyCyqHCt4eiEE0mmyuzgd143L7EM" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
 global.BOT_NAME = "Alphabot-Mdོ" //your bot name
 global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
 global.OWNER_NUMBER = "62887435047326" //your telegram number
